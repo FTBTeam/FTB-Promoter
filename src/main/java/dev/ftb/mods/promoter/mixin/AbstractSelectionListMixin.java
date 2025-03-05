9864b464dfaf299410152e22d75c3baf09266672
Michael Hillcox <michaelhillcox@gmail.com>
2025-03-05 09:47:37 +0000

    fix: mixin issues again (for published version)

--- src/main/java/dev/ftb/mods/promoter/mixin/AbstractSelectionListMixin.java
+++ src/main/java/dev/ftb/mods/promoter/mixin/AbstractSelectionListMixin.java
@@ -3,20 +3,14 @@ package dev.ftb.mods.promoter.mixin;
 import dev.ftb.mods.promoter.integrations.Integrations;
 import net.minecraft.client.gui.components.AbstractSelectionList;
 import net.minecraft.client.gui.screens.multiplayer.ServerSelectionList;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.List;
-
 @Mixin(AbstractSelectionList.class)
 public abstract class AbstractSelectionListMixin<E extends AbstractSelectionList.Entry<E>> {
-    @Shadow @Final private List<E> children;
-
     @Inject(method = "addEntry", at = @At("HEAD"), cancellable = true)
     private void addEntry(E entry, CallbackInfoReturnable<Integer> cir) {
         @SuppressWarnings("unchecked")
@@ -28,7 +22,7 @@ public abstract class AbstractSelectionListMixin<E extends AbstractSelectionList
         }
 
         if (Integrations.denyEntry(entry)) {
-            cir.setReturnValue(this.children.size() - 1);
+            cir.setReturnValue(self.children().size() - 1);
         }
     }
 
