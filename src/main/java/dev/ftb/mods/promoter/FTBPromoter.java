7fa096681cf0f77c80eabc6178ee856208cf5bb0
Michael Hillcox <michaelhillcox@gmail.com>
2025-04-08 22:21:06 +0100

    fix: don't crash on server start

--- src/main/java/dev/ftb/mods/promoter/FTBPromoter.java
+++ src/main/java/dev/ftb/mods/promoter/FTBPromoter.java
@@ -1,9 +1,11 @@
 package dev.ftb.mods.promoter;
 
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -16,6 +18,10 @@ public class FTBPromoter {
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public FTBPromoter() {
+        if (FMLEnvironment.dist != Dist.CLIENT) {
+            return;
+        }
+
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         modEventBus.addListener(this::clientSetup);
     }
